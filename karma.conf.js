aa32b8a919c65c504e4ca35784d32f24e0298808
Fajar Tri Saputra <fajartrisaputra@gmail.com>
2020-01-30 00:45:51 +0700

    add firebase cli

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-material7-firebase-crud-app'),
+      dir: require('path').join(__dirname, './coverage/backend-dejavu'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
